0f5d3f858471c6470422bd097e557ffc13212138
KARTIKEY KUMAR <kartikeyk91@gmail.com>
2025-08-29 03:30:26 +0530

    render url

--- Frontend/src/API/api.js
+++ Frontend/src/API/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:4001", // ✅ coming from .env
+  baseURL: import.meta.env.VITE_API_URL || "https://bookstore-app-tpaq.onrender.com", // ✅ coming from .env
   withCredentials: true, // needed if using cookies/sessions
 });
 export default API;
